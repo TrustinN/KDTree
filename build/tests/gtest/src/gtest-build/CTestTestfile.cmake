97e359c07da0f3c822fdb462eb67036855f2d672
Trustin Nguyen <trustin.nguyen62@berkeley.edu>
2024-11-16 17:01:31 -0800

    ...

--- build/tests/gtest/src/gtest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/trustinnguyen/Downloads/Programming/CPP/KDTree/build/tests/gtest/src/gtest
-# Build directory: /Users/trustinnguyen/Downloads/Programming/CPP/KDTree/build/tests/gtest/src/gtest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
